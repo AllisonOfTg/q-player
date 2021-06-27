1fb179e04af2f93022e3d6cb4e4d780714d662a2
Nick Fury <73124813+NickxFury@users.noreply.github.com>
2021-06-27 13:22:36 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -28,12 +28,8 @@ from config import Config
 REPLY_MESSAGE=Config.REPLY_MESSAGE
 buttons = [
     [
-        InlineKeyboardButton('⚡️Make Own Bot', url='https://heroku.com/deploy?template=https://github.com/subinps/MusicPlayer'),
-        InlineKeyboardButton('🧩 Source Code', url='https://github.com/subinps/MusicPlayer'),
-    ],
-    [
-        InlineKeyboardButton('🎧Play Music', url=f'https://t.me/{USERNAME}'),
-        InlineKeyboardButton('👨🏼‍🦯 Help', callback_data='help')       
+        InlineKeyboardButton('📜 Channel', url='https://t.me/MovieClubOfficiall'),
+        InlineKeyboardButton('Group ♻️', url='https://t.me/MOVIECLUB_CHAT)      
     ]
     ]
 @Client.on_inline_query()
@@ -43,7 +39,7 @@ async def search(client, query):
         answers.append(
             InlineQueryResultArticle(
                 title="Deploy",
-                input_message_content=InputTextMessageContent(f"{REPLY_MESSAGE}\n\n<b>You can't use this bot in your group, for that you have to make your own bot from the [SOURCE CODE](https://github.com/subinps/MusicPlayer) below.</b>", disable_web_page_preview=True),
+                input_message_content=InputTextMessageContent(f"{REPLY_MESSAGE}\n\n<b>You can't use this bot in your group/channel.\nJoin Our Movie Group By Clicking The Links Below.</b>", disable_web_page_preview=True),
                 reply_markup=InlineKeyboardMarkup(buttons)
                 )
             )
